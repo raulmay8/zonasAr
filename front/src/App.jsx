d633c19fd6f2c800b915a68f4bfc561c7649a7b2
raul8888 <raulmay8@hotmail.com>
2023-11-27 14:48:22 -0500

    ultimo v2

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Inicio from './Pages/Inicio'
 import Estado from './Pages/Estado'
@@ -7,21 +6,12 @@ import Mejores from './Pages/Mejores'
 import Header from './components/Header'
 import DetalleEstado from './components/DetalleEstado'
 import DetalleZona from './components/DetalleZona'
-function App() {
-=======
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Inicio from './Pages/Inicio';
-import Estado from './Pages/Estado';
-import Nosotros from './Pages/Nosotros';
-import Mejores from './Pages/Mejores';
->>>>>>> 2fce085827688a4c221bbd45b11bee1954fdc1b1
 
 function App() {
   return (
     <BrowserRouter>
     <Header/>
       <Routes>
-<<<<<<< HEAD
         <Route path='/' element={<Inicio/>}/>
         <Route path='/estado' element={<Estado/>}/>
         <Route path='/estado/:id' element={<Estado/>}/>
@@ -30,13 +20,6 @@ function App() {
         <Route path='/mejores' element={<Mejores/>}/>
         <Route path='/detalle/:id' element={<DetalleEstado/>}/>
         <Route path='/detalle/zona/:id' element={<DetalleZona/>}/>
-=======
-        <Route path="/" element={<Inicio />} />
-        <Route path="/estado" element={<Estado />} />
-        <Route path="/nosotros" element={<Nosotros />} />
-        <Route path="/mejores/:id" element={<Mejores />} />
-        <Route path="/mejores" element={<Mejores />} />
->>>>>>> 2fce085827688a4c221bbd45b11bee1954fdc1b1
       </Routes>
     </BrowserRouter>
   );
